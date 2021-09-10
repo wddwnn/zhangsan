67e1ea2bd4e27ca644725c20a9d72d7ae8e9aa2a
wddwnn <1374114183@qq.com>
2021-09-10 14:20:49 +0800

    9月10日第二节结束

--- demo01.py
+++ demo01.py
@@ -29,28 +29,78 @@
 
 #2.数组
 #append（）追加，尾巴上加
-a=[1,2,3,4,"哈哈","嘻嘻",True,False]
-print(a)
-a.append("你好")
-print(a)
+# a=[1,2,3,4,"哈哈","嘻嘻",True,False]
+# print(a)
+# a.append("你好")
+# print(a)
 
 #insert()想插入到哪就到哪
-a.insert(1,"insert")#1是插入的位置
-print(a)
+# a.insert(1,"insert")#1是插入的位置
+# print(a)
 
 #pop()剪切的作用：把这个值从数据中拿出来，可赋值给新的变量
-b=a.pop(5)
-print(b)
+# b=a.pop(5)
+# print(b)
 
 #clear()清空
 #copy()复制，测试里比较少用
 #extend()与append()作用相同，区别在于一次可以放入多个值
-a.extend(["哈喽","嗨呀"])#记得加[]
-print(a)
+# a.extend(["哈喽","嗨呀"])#记得加[]
+# print(a)
 
 #a.remove("哈哈")把哈哈删除
 #a.remove(0)把false删除
 #a.remove(1)把true删除
+
+#字典/dict，字典中的值没有顺序，结构必须是键值对：key:value
+# a={
+#     "name":"张三",
+#     0:"哈哈",
+#     "age":25
+#     }
+# print(a)
+
+#写key,把value取出
+# print(a["name"])#取值
+# a["high"]="183cm"#新增
+# print(a)
+# a["name"]="李四"#修改
+# print(a)
+
+#get()取值
+#pop()剪切
+#update()更新同修改，key 存在就修改，不存在就新增
+# a.update(name=1111)
+# a.update(weight=65)
+# print(a)
+
+#数组与字典的删除：字典：del a["name"]   数组：del a[0]
+# del a["age"]
+# print(a)
+
+#练习 获取用户输入的个人信息，并且存储到字典中
+#个人信息包括：name,age,sex
+a=input("请输入个人信息：name:")
+b={"name":a}
+a=input("age:")
+b["age"]=a
+a=input("sex:")
+b["sex"]=a
+print(b)
+
+
+"""
+a={ "name":"张三",
+    "外号":"哈哈",
+    "age":25,
+    "high":"180cm",
+    "sex":"男",
+    "hobby":"打篮球，下象棋
+    }
+"""
+
+
+
 
 
 
